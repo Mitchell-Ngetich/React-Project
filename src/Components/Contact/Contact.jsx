3d025100d27f66c61e53d0f04263e04032b6b618
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-04-27 23:30:08 +0300

    Added Footer

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,23 +1,35 @@
+import { queryAllByAttribute } from '@testing-library/react';
 import React, { useRef } from 'react';
 import './Contact.css';
 
 const Contact = () => {
-    const form = useRef()
+    const form = useRef();
+
+     function handleSubmit(e) {
+       e.preventDefault();
+       alert("Sent");
+     }
+
   return (
     <div className="container contact__container">
-    <form ref={form} >
-      <input type="text" name="name" placeholder="Your Full Name" required />
-      <input type="email" name="email" placeholder="Your Email" required />
-      <textarea
-        name="message"
-        rows="7"
-        placeholder="Your Message"
-        required
-      ></textarea>
-      <button type="submit" className="btn btn-primary">
-        Send Message
-      </button>
-    </form>
+      <form ref={form}>
+        <input type="text" name="name" placeholder="Your Full Name" />
+        <input type="email" name="email" placeholder="Your Email" required />
+        <textarea
+          name="message"
+          rows="7"
+          placeholder="Your Message"
+          required
+        ></textarea>
+        <button
+          type="submit"
+          onClick={() => alert("Sent")}
+          required
+          className="btn btn-primary"
+        >
+          Send Message
+        </button>
+      </form>
     </div>
   );
 }
